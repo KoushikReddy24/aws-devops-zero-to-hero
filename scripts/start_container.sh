1a24298ef13df980c1a858cd94086c895f6b7653
KoushikReddy24 <124913094+KoushikReddy24@users.noreply.github.com>
2024-05-28 19:24:46 +0530

    version of docker image

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull koushikreddy24/myflaskapp:2
+docker pull koushikreddy24/myflaskapp
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 koushikreddy24/myflaskapp:2
+docker run -d -p 5000:5000 koushikreddy24/myflaskapp
 
